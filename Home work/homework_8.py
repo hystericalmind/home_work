ab845d82ca6073c2ff8f6850b53fc6c936b7711d
hystericalmind <standarts@inbox.ru>
2024-04-06 23:07:56 +0300

    Как то не до конца могу понять всю суть цикла for. МОжет есть где почитать больше информации по этому моменту ?

---
+++ Home work/homework_8.py
@@ -0,0 +1,9 @@
+cars_ = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
+for i in cars_:
+    print ('Я езжу на автомобиле марки :', i)
+for j in cars_:
+    cars_count += 10
+    print('Я езжу на автомобиле марки :', j, cars_count)
+
+
