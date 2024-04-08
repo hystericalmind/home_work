76b976c34d0f5c9ca5c59614ffc263f3ccb4c9f0
hystericalmind <standarts@inbox.ru>
2024-04-08 23:49:01 +0300

    как вам такой вариант выполнения задания?

--- Home work/rework_01.py
+++ Home work/rework_01.py
@@ -1,16 +1,14 @@
-def print_params(Home, *z):
-    print(f'Home number: {Home}')
-    for i in z:
-        number = [267, 204, 17]
-    if i == 267:
-        print(f'Total area:', {i})
-
-print()
-
-
-
-print_params(200,*[267])
-print_params(200,*[666])
+def function_one(num_1,*num_2):
+    num_1 = int(input('input number 1: '))
+    print(f'Your num #1 is : {num_1}!')
+    for i in num_2:
+        num_2 = int(input('i need number 2, now!!! input: '))
+        if num_2 > 1000:
+            print(num_1 * num_2)
+        elif num_2 < 1000:
+            print('Error')
 
 
 
+function_one(1, 1)
+function_one(1, 1, 1, 1)
