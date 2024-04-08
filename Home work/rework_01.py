3d85c3833e70e49e976ecd236ba51ba8a6820cdc
hystericalmind <standarts@inbox.ru>
2024-04-08 20:59:19 +0300

    в таком варианте пойдет?

---
+++ Home work/rework_01.py
@@ -0,0 +1,16 @@
+def print_params(Home, *z):
+    print(f'Home number: {Home}')
+    for i in z:
+        number = [267, 204, 17]
+    if i == 267:
+        print(f'Total area:', {i})
+
+print()
+
+
+
+print_params(200,*[267])
+print_params(200,*[666])
+
+
+
