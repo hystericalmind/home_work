6e29a5389a470967916979286945374a08712f1a
hystericalmind <standarts@inbox.ru>
2024-04-09 18:22:23 +0300

    Я переделал. Если опять не правильно, то скорее всего я не понимаю "ТЗ"

--- Home work/rework_01.py
+++ Home work/rework_01.py
@@ -1,15 +1,20 @@
-def function_one(num_1,*num_2):
-    num_1 = int(input('input number 1: '))
-    print(f'Your num #1 is : {num_1}!')
-    for i in num_2:
-        num_2 = int(input('i need number 2, now!!! input: '))
-        if num_2 > 1000:
-            print(num_1 * num_2)
-        elif num_2 < 1000:
-            print('Error')
+import random
 
 
+def print_params(param):
+    list_0 = list(range(2, 20))
+    for i in list_0:
+        args_1 = random.choice(list_0)
+        list_0.remove(args_1)
+        args_2 = random.choice(list_0)
+        list_0.remove(args_2)
 
-function_one(1, 1)
-function_one(1, 1, 1, 1)
+        print(
+            f'Было выбрано сдучайное число : {args_1}. Был ввенден аргумен: {param}. Был умножен параметр на элемент списка {args_1 * param}')
+        print(
+            f'Было: выбрано случайное число : {args_2}. Был ввенден аргумен: {param}. Был умножен параметр на элемент списка: {args_2 * param}')
 
+
+        break
+
+print_params(300)
