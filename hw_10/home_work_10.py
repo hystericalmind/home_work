4337b93ae4cd28935fcc360970fdaada2c975a3e
hystericalmind <standarts@inbox.ru>
2024-04-24 20:05:37 +0300

    Репа в ручную

---
+++ hw_10/home_work_10.py
@@ -0,0 +1,6 @@
+def hi_1():
+    print("Hello, World")
+
+def hi_2():
+    print("Hello, World")
+
---
+++ hw_10/main.py
@@ -0,0 +1,5 @@
+from hw_10 import home_work_10 as import_f
+
+import_f.hi_1()
+import_f.hi_2()
+giy
