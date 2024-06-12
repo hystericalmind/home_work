09f4ea256f5f89b9b0c0c68a4c68642571a48f98
hystericalmind <standarts@inbox.ru>
2024-06-12 16:44:48 +0300

    Домашнее задание Наследование классов

---
+++ Самостоятельная практика/classes/home_work_сlass_inheritance.py
@@ -0,0 +1,28 @@
+class Car:
+    price = 1000000
+
+    def horse_powers(self, hp):
+        print(f'HP = {hp}')
+class Nissan(Car):
+    pass
+    price = 2000000
+
+    def horse_powers(self, hp):
+        print(f'This is Nissan, price = {self.price}, my HP = {hp}')
+
+
+class Kia(Car):
+    price = 900000
+    def horse_powers(self, hp):
+        print(f'This is KIA, price = {self.price} my HP = {hp}')
+
+
+
+
+car = Car()
+nissan = Nissan()
+kia = Kia()
+
+car.horse_powers(149)
+nissan.horse_powers(200)
+kia.horse_powers(115)
