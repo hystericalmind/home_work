7916c250dce573fdb3b8c04b3c9eb53bff6b99de
hystericalmind <standarts@inbox.ru>
2024-06-17 23:13:31 +0300

    Доступ к свойствам родителя. Переопределение свойств

--- Самостоятельная практика/classes/home_work_multipan_inheritence.py
+++ Самостоятельная практика/classes/home_work_multipan_inheritence.py
@@ -1,21 +1,35 @@
 class Vihicle:
     vehicle_type = None
     def __init__(self,vehicle_type):
-        self.price = vehicle_type
+        pass
 
 class Car:
     price = 1000000
     def __init__(self,price):
-        self.price = price
+        pass
     def horse_powers(self, hp):
         self.hp = hp
-        return  hp
+        return hp
 
 class Nissan(Car, Vihicle):
-    def __init__(self, vehicle_type, price):
+    def __init__(self,vehicle_type, price, ):
+        super().__init__(Car.horse_powers)
         super().__init__(vehicle_type)
+        self.vehicle_type = 999
         super().__init__(price)
-        super().horse_powers(150)
+        # super().horse_powers(self.hp)
+        # super().horse_powers(self.hp)
 
-nissan = Nissan(True, 90000000)
+    def horse_powers(self, hp):
+        super().horse_powers(hp)
+        self.hp = ('переопределил получается ?')
+        print(self.hp)
+
+
+
+
+
+
+nissan = Nissan('ни че не меняется', 90000000)
+print(nissan.horse_powers(200))
 print(nissan.vehicle_type, nissan.price)
