c378cae8b95f44ed8d4341a83ba6d5e8c4b5ce9e
hystericalmind <standarts@inbox.ru>
2024-06-17 23:16:04 +0300

    Переделал задание, переопределил метод. Правильно?

--- Самостоятельная практика/classes/home_work_multipan_inheritence.py
+++ Самостоятельная практика/classes/home_work_multipan_inheritence.py
@@ -2,7 +2,6 @@ class Vihicle:
     vehicle_type = None
     def __init__(self,vehicle_type):
         pass
-
 class Car:
     price = 1000000
     def __init__(self,price):
@@ -10,26 +9,17 @@ class Car:
     def horse_powers(self, hp):
         self.hp = hp
         return hp
-
 class Nissan(Car, Vihicle):
     def __init__(self,vehicle_type, price, ):
         super().__init__(Car.horse_powers)
         super().__init__(vehicle_type)
         self.vehicle_type = 999
         super().__init__(price)
-        # super().horse_powers(self.hp)
-        # super().horse_powers(self.hp)
-
     def horse_powers(self, hp):
         super().horse_powers(hp)
         self.hp = ('переопределил получается ?')
         print(self.hp)
-
-
-
-
-
-
 nissan = Nissan('ни че не меняется', 90000000)
 print(nissan.horse_powers(200))
 print(nissan.vehicle_type, nissan.price)
+
