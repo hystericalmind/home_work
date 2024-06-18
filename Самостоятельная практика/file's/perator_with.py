37a155908466e1599472dc703214636952d86154
hystericalmind <standarts@inbox.ru>
2024-06-18 23:17:38 +0300

    Домашняя работа "Оператор  with"

---
+++ Самостоятельная практика/file's/perator_with.py
@@ -0,0 +1,12 @@
+file_name = 'my_first_file'
+file = open(file_name, mode='r',encoding='utf8')
+file_data = file.read()
+while file_data:
+    if 'my' in file_data:
+        print(file_data)
+        break
+file.close()
+
+
+
+
