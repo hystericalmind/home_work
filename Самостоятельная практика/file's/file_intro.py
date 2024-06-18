8bb8b76c3a531a57a80ee104b2548f6e7ec3dfc1
hystericalmind <standarts@inbox.ru>
2024-06-18 23:10:01 +0300

    Домашняя работа по оператору while

--- Самостоятельная практика/file's/file_intro.py
+++ Самостоятельная практика/file's/file_intro.py
@@ -1,5 +1,15 @@
 file_name = 'my_first_file'
 file = open(file_name, mode='r',encoding='utf8')
 file_data = file.read()
+while file_data:
+    if 'my' in file_data:
+        print(file_data)
+        break
 file.close()
-print(file_data)
+
+
+
+
+
+# Создайте переменную с этим файлом
+# Распечатайте содержимое текстового файла в консоль, используя оператор with
