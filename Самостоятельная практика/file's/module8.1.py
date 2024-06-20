c16b067a7dbde3fe302840e353fcf35cef4d591f
hystericalmind <standarts@inbox.ru>
2024-06-21 00:15:29 +0300

    Домашняя работа "Try и Except"

--- Самостоятельная практика/file's/module8.1.py
+++ Самостоятельная практика/file's/module8.1.py
@@ -1,6 +1,7 @@
 
 
 def add_everything_up(a, b):
+    # result = a + b
     return (f'{a},{b}')
 
 
@@ -9,17 +10,16 @@ b = str
 
 
 try:
-    a == str
-
+    a != int and a != float
 except(TypeError):
     print("input data - type error(non int or float)")
 try:
-    b == (int or float)
+    b != str
 except(TypeError):
     print("input data - type error(non str)")
-finally:
-    pass
+
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
+
