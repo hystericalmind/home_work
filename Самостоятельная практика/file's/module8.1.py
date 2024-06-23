02f8e0db84c827d60f2babbb4725cdc169037924
hystericalmind <standarts@inbox.ru>
2024-06-23 13:47:42 +0300

    Домашняя работа "try & except", исправленное 2

--- Самостоятельная практика/file's/module8.1.py
+++ Самостоятельная практика/file's/module8.1.py
@@ -2,8 +2,9 @@ def add_everything_up(a, b):
     try:
         result = (a + b)
     except TypeError:
-        result_2 = (a,b)
-        return str(result_2)
+        a = str(a)
+        b = str(b)
+        return a + b
     else:
         return a+b
 
@@ -11,5 +12,4 @@ def add_everything_up(a, b):
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
-
 
