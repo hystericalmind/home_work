d0ffc5f22a1b60cfb3cfd261acc0846380b41360
hystericalmind <standarts@inbox.ru>
2024-07-02 18:40:29 +0300

    Домашняя работа "Итераторы"

--- Самостоятельная практика/function_prog/Iterator's.py
+++ Самостоятельная практика/function_prog/Iterator's.py
@@ -8,7 +8,7 @@ class EvenNumbers:
         self.end = end
         self.i = 0
 
-    def __call__(self):
+    def __iter__(self):
         self.i = 0
         return self
 
@@ -16,37 +16,24 @@ class EvenNumbers:
         list_1 = []
         if self.start < self.end:
             for i in range(self.start, self.end):
-                if i % 2:
+                if i % 2 == 0:
                     list_1.append(i)
-                    return list_1
-        self.i += 1
+                    continue
+            return list_1
         if self.start == None :
             self.start = 0
-            return self.start
-        else:
             return self.start
         if self.end == None:
             self.end = 1
             return self.end
-        else:
-            return self.end
+
         raise StopIteration()
-
-        # def iterate_1(self):
-            # list_1 = []
-            # if self.start < self.end:
-            #     for i in range(self.start, self.end):
-            #         if i % 0:
-            #             list_1 += i
-            #             print(list_1)
-
 
 
 
 result = EvenNumbers(10,25)
 test = result.__next__()
 print()
-
-print(result)
-
+for i in test:
+    print(i)
 
