a9988eaae6f8631fd372c5050de853279cf98982
hystericalmind <standarts@inbox.ru>
2024-07-11 21:22:52 +0300

    Домашняя работа "Декораторы енераторы" с новыми исправлениями

--- Самостоятельная практика/function_prog/module_8_4.py
+++ Самостоятельная практика/function_prog/module_8_4.py
@@ -1,15 +1,17 @@
 def all_variants(text):
-    res_1 = ''
+    leight = len(text)
     for i in text:
         yield i
-    if len(text) > 0 :
-        yield (f'{text[0]}{text[1]}\n{text[1]}{text[2]}')
-    for k in text:
+    for start in range(leight):
+        pass
+    for end in range(start + 1, leight + 1):
+        yield (f'{text[:2]}\n{text[-2:]}')
+    for k in range(1):
         yield text
-        break
 
-
+        
 func_txt = all_variants(text='abc')
+
 
 for res in func_txt:
     print(res)
