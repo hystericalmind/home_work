55b54f210a4bc2d98766a5ab9b0ffb312d15512c
hystericalmind <standarts@inbox.ru>
2024-07-22 23:32:51 +0300

    Домашняя работа modul_2_hard

--- Самостоятельная практика/module2hard.py
+++ Самостоятельная практика/module2hard.py
@@ -1,16 +1,17 @@
 import random
 
-n = random.randint(3, 20) - 1
+n = random.randint(3, 20)
+
 
 result = []
 
 def generate_password(n):
     global result
-    for i in range(1, n):
-        for j in range(1, n):
-            if (j + i) % n == 0:
-                result += (i,j)
+    for i in range(1, n+1):
+        for j in range(1, i):
+            if n % (i + j) == 0:
+                result +=(i,j)
 
 generate_password(n)
 
-print(f' Выпавшее число: {n}, Пароль: {result}')
+print(f' Выпавшее число: {n}, Пароль: {result[::-1]}')
